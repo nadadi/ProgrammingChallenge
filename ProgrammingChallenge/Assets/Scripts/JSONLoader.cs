c09ac9628537f3f6a85caeddf34d8114a7922a79
Nathal <nathaldawsondiaz@gmail.com>
2020-05-22 10:33:17 -0400

    It has been added the reload data functionality

--- ProgrammingChallenge/Assets/Scripts/JSONLoader.cs
+++ ProgrammingChallenge/Assets/Scripts/JSONLoader.cs
@@ -4,12 +4,17 @@ using UnityEngine.UI;
 
 public class JSONLoader : MonoBehaviour
 {
+    #region CLASS_VARIABLES
+    public GameObject panel;
     public Text title;
-    public GridLayoutGroup grid;
     public GameObject fieldPrefab;
     private string jsonPath;
     private TableContent content;
+    private GameObject grid;
+    private GridLayoutGroup gridLayout;
+    #endregion
 
+    #region UNITY_METHODS
     private void Awake()
     {
         jsonPath = Application.streamingAssetsPath + "/JsonChallenge.json";
@@ -19,25 +24,66 @@ public class JSONLoader : MonoBehaviour
     {
         LoadData();
     }
+    #endregion
 
-    private void LoadData()
+    #region CLASS_METHODS
+    public void LoadData()
     {
         using (StreamReader stream = new StreamReader(jsonPath))
         {
             string json = stream.ReadToEnd();
             content = JsonUtility.FromJson<TableContent>(json);
         }
-        ShowDataOnUI();
+
+        if (content != null)
+        {
+            CleanGridLayout();
+            ShowDataOnUI();
+        }
     }
 
     private void ShowDataOnUI()
     {
         // Load the title 
         title.text = content.Title;
-        // Constraint the grid for data
-        grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
-        grid.constraintCount = content.ColumnHeaders.Length;
+        // Creates grid, adding a grid layout and constraint for data
+        CreateGrid();
+
         // Load the headers of the file
+        AssignHeaders();
+
+        // Load the members data
+        LoadContent();
+    }
+
+    private void CleanGridLayout()
+    {
+        Destroy(grid);
+    }
+
+    private void CreateGrid()
+    {
+        grid = new GameObject("Grid");
+        grid.transform.parent = panel.transform;
+        RectTransform rectTransform = grid.AddComponent<RectTransform>();
+        rectTransform.offsetMin = new Vector2(0f, -0.0001220703f);
+        rectTransform.offsetMax = new Vector2(0f, -1.924133f);
+        rectTransform.anchorMin = new Vector2(0f, 0f);
+        rectTransform.anchorMax = new Vector2(1f, 0.881f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        gridLayout = grid.AddComponent<GridLayoutGroup>();
+        gridLayout.padding.left = 100;
+        gridLayout.padding.right = 100;
+        gridLayout.cellSize = new Vector2(100, 50);
+        gridLayout.startCorner = GridLayoutGroup.Corner.UpperLeft;
+        gridLayout.startAxis = GridLayoutGroup.Axis.Horizontal;
+        gridLayout.childAlignment = TextAnchor.UpperLeft;
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = content.ColumnHeaders.Length;
+    }
+
+    private void AssignHeaders()
+    {
         foreach (var header in content.ColumnHeaders)
         {
             GameObject field = Instantiate(fieldPrefab);
@@ -46,7 +92,10 @@ public class JSONLoader : MonoBehaviour
             text.text = header;
             text.fontStyle = FontStyle.Bold;
         }
-        // Load the members data
+    }
+
+    private void LoadContent()
+    {
         foreach (var data in content.Data)
         {
             GameObject id = Instantiate(fieldPrefab);
@@ -66,4 +115,5 @@ public class JSONLoader : MonoBehaviour
             nickname.GetComponent<Text>().text = data.Nickname;
         }
     }
+    #endregion
 }
